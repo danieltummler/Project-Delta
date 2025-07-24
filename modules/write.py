51a563199a1a5d75ea7a0439407687ac24136e20
daniel.tummler <49766769+danieltummler@users.noreply.github.com>
2025-07-24 11:31:05 +0200

    Update write.py

--- modules/write.py
+++ modules/write.py
@@ -14,7 +14,8 @@ def show_cocktail_matrix(book):
 
     selection = st.pills(label = "Cocktails",
                          options = list_books,
-                         selection_mode = "single")
+                         selection_mode = "single",
+                         default = 0)
     
     cocktail_info = [cocktail for cocktail in book if cocktail["name"] == selection][0]
 
