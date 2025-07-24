530b9805dbdfb593f6a6f6f6d0de5cbc997c3115
daniel.tummler <49766769+danieltummler@users.noreply.github.com>
2025-07-24 17:30:26 +0200

    Update read.py

--- modules/read.py
+++ modules/read.py
@@ -4,17 +4,21 @@ import pickle
 import os
 import streamlit as st
 
-def read_book(book_name = "experimental_cocktail_club_book.pkl"):
+def read_book():
+
+    book_name = st.selectbox(label = "Book Name",
+                             options = ["experimental_cocktail_club_book.pkl", "fenix_cocktail_bar_book.pkl"],
+                             index = 1)
 
     # books = os.listdir()
   
-    # if book_name in books:
+    if book_name:
 
-    with open(file = book_name, mode = "rb") as file:
-        book = pickle.load(file = file)
+        with open(file = book_name, mode = "rb") as file:
+            book = pickle.load(file = file)
 
-    # else:
-    #     raise Exception("No Book.")
+    else:
+        raise Exception("No Book.")
 
     return book
 
