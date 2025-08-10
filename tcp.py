3a6f3391605fe10ee72b03627bb436d8bc4e2998
daniel.tummler <49766769+danieltummler@users.noreply.github.com>
2025-08-10 02:53:02 +0200

    Add files via upload

--- tcp.py
+++ tcp.py
@@ -1,26 +1,58 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
+import os
 from modules import read
 from modules import write
 
+st.set_page_config(page_title = " ∇ The Cocktail Project", page_icon = "∇", layout = "wide")
+
 def main():
 
-    st.markdown("# $\\nabla$")
+    st.markdown(body = "# $\\nabla$")
 
-    book = read.read_book()
+    # Queda presentable, pero no hace referencia al archivo original
+    # cocktail_books = [book.split("_", maxsplit = 1)[1].strip(".pkl").replace("_", " ").title() for book in os.listdir("cocktail_books")]
 
-    # with st.sidebar:
-    #     write.show_list_cocktails_from_book(book)
+    cocktail_books = [book for book in os.listdir("cocktail_books")]
+    book_selection = st.sidebar.selectbox(options = cocktail_books, label = "Cocktail Books")
 
-    cocktail_info = write.show_cocktail_matrix(book = book)
+    book = read.read_book(book_selection = book_selection)
 
-    write.show_cocktail(cocktail_info = cocktail_info)
-    try:
-        write.show_picture(cocktail_info = cocktail_info)
-    except:
-        st.write("No pictures.")
+    book_menu = st.segmented_control(label = "Menu", options = ["Cocktails", "Homemade Ingredients", "Charts"])
 
+    if book_menu == "Cocktails":
+        st.markdown(body = f"### {book_selection.split('_', maxsplit = 1)[1].strip('.pkl').replace('_', ' ').title()}")
+
+        cocktails = book["cocktails"]
+
+        col1, col2, col3, col4 = st.columns([1 for x in range(4)])
+
+        for i in range(0, len(cocktails), 4):
+
+            with col1.popover(f"{cocktails[i].get('name')}"):
+                write.ecc_popover_info(cocktails[i])
+            try:
+                with col2.popover(f"{cocktails[i + 1].get('name')}"):
+                    write.ecc_popover_info(cocktails[i + 1])
+            except:
+                pass
+
+            try:
+                with col3.popover(f"{cocktails[i + 2].get('name')}"):
+                    write.ecc_popover_info(cocktails[i + 2])
+            except:
+                pass
+
+            try:
+                with col4.popover(f"{cocktails[i + 3].get('name')}"):
+                    write.ecc_popover_info(cocktails[i + 3])
+            except:
+                pass
+
+    elif book_menu == "Homemade Ingredients":
+        st.markdown(body = "### Homemade Ingredients")
+
+    else:
+        pass
 
 
 if __name__ == "__main__":
